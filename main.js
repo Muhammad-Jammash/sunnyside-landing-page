22a630c168e2d10539f4dc48200f54521ddde7df
Muhammad-Jammash <jammashparacha414@gmail.com>
2022-02-03 03:17:02 +0500

    Sunnyside-landing-page

--- main.js
+++ main.js
@@ -1,23 +1,8 @@
 var hamburger = document.getElementById('hamburger');
-var desktopMenu = document.getElementById('desktop-menu');
 var mobileMenu = document.getElementById('mobile-menu');
 
-document.getElementById('hamburger').addEventListener("click", toggleMenu);
-
+hamburger.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    if (mobileMenu.style.display == 'block') {
-        mobileMenu.style.display = 'none';
-    }
-    else {
-        mobileMenu.style.display = 'block';
-    }
+    mobileMenu.classList.toggle('active');
 }
-
-function windowChange() {
-    if(window.matchMedia("(min-width: 768px)").matches) {
-        mobileMenu.style.display = 'none';
-    }
-}
-
-windowChange();
